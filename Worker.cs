752d405a7c8fbc6b3da4bd113de4ae9a5dac2bc0
Tom Strausbaugh <tstrausbaugh@straustech.net>
2021-09-01 14:58:02 -0400

    Fetching Album data from directory structure

--- Worker.cs
+++ Worker.cs
@@ -5,14 +5,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using NetCoreAudio;
-using NetCoreAudio.Interfaces;
+using System.IO;
 
 namespace StrausRadio
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private static Random rng = new Random();
+        // TODO: Add Extension filter to settings
+        private List<string> AudioExtensions = new List<string>() { ".mp3", ".flac" };
 
         public Worker(ILogger<Worker> logger)
         {
@@ -21,24 +23,67 @@ namespace StrausRadio
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            /*while (!stoppingToken.IsCancellationRequested)
+            while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
 
-            }*/
+                var randomAlbums = GetAlbums();
 
-
-            var player = new Player();
-
-            player.PlaybackFinished += OnPlaybackFinished;
-
-            await player.Play(@"C:\Users\Tom\Desktop\test.mp3");
+                foreach (var album in randomAlbums)
+                {
+                    foreach (var track in album.Tracks)
+                    {
+                        // track.FullPath;
+                    }
+                }
+            }
         }
 
-        private void OnPlaybackFinished(object sender, EventArgs e)
+        private List<Album> GetAlbums()
         {
-            Console.WriteLine("Playback finished");
+            var results = new List<Album>();
+
+            DirectoryInfo musicDir = new DirectoryInfo(@"\\cerebrum\music");
+
+            var artistDirs = musicDir.EnumerateDirectories();
+
+            foreach (var artistDir in artistDirs)
+            {
+                var albumDirs = artistDir.EnumerateDirectories();
+
+                foreach (var albumDir in albumDirs)
+                {
+                    var album = new Album();
+
+                    album.Artist = artistDir.Name;
+                    album.Name = albumDir.Name;
+
+                    var tracks = albumDir.EnumerateFiles();
+
+                    album.Tracks = tracks.Where(t => AudioExtensions.Contains(t.Extension))
+                        .Select(t => new Track() { Name = t.Name, Extension = t.Extension, FullPath = t.FullName })
+                        .ToList();
+
+                    results.Add(album);
+                }                
+            }
+
+            return results.OrderBy(r => rng.Next()).ToList();
         }
+    }
+
+    public struct Album
+    {
+        public string Name;
+        public string Artist;
+        public List<Track> Tracks;
+    }
+
+    public struct Track
+    {
+        public string Name;
+        public string Extension;
+        public string FullPath;
     }
 }
